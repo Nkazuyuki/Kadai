fd802e801c045e3f7d7f922e397da553fe37560c
Nkazuyuki <nameless5life@gmail.com>
2018-06-16 15:27:58 +0900

    Excercise3終了

---
+++ Excercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Excercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字を入力:");
+            var inputText = Console.ReadLine();
+            Console.WriteLine("入力された文字を表記：" + inputText);
+        }
+    }
+}
